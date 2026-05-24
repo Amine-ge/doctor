502c8ddf4ece83e679a14f2ecda1f27afd25023d
Grance <2367889165@qq.com>
2026-05-24 13:12:35 +0800

    测试

--- ruoyi-admin/src/main/java/com/ruoyi/ai/controller/PublicBanners.java
+++ ruoyi-admin/src/main/java/com/ruoyi/ai/controller/PublicBanners.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 import static com.ruoyi.common.utils.PageUtils.startPage;
@@ -17,6 +17,15 @@ import static com.ruoyi.common.utils.PageUtils.startPage;
 @RequestMapping("/api/banners")
 public class PublicBanners extends BaseController {
 
+    @PostConstruct
+    public void init() {
+        System.out.println("PublicBanners Controller loaded");
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "banners ok";
+    }
    @Autowired
    private IAiBannersService aiBannersService;
     @GetMapping("/list")
